208213745114fdd0d6a6be41af783d2a6bee7170
iskvmksm <isakovmaksim777@gmail.com>
2021-05-13 16:21:51 +0600

    create: Added reviews section fix: hotel slider

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
-const swiper = new Swiper('.swiper-container', {
+const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
   },
    keyboard: {
     enabled: true,
     onlyInViewport: false,
    }
 });
-ymaps.ready(init);
-  function init(){
-    var myMap = new ymaps.Map("map", {
-      center: [55.833057, 37.484996],
-      zoom: 15});
-    var myGeoObject = new ymaps.GeoObject({});
-    myMap.geoObjects
-    .add(myGeoObject)
-    .add(new ymaps.Placemark([55.833057, 37.484996]))
-  
-  }
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+   keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+   }
+});
