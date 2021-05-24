95625bac31348bd5006ee8b4a770a547c031b426
iskvmksm <isakovmaksim777@gmail.com>
2021-05-24 16:10:26 +0600

    fix: modal popapp

--- js/main.js
+++ js/main.js
@@ -42,10 +42,12 @@ $(document).ready(function() {
   modalButton.on('click', openModal);
 
   function openModal() {
-    var targetModal = $(this).attr("data-href");
-    $(targetModal).find('.modal__overlay').addClass("modal__overlay--visible");
-    $(targetModal).find('.modal__dialog').addClass("modal__dialog--visible");
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
   }
+
   function closeModal(event) {
     event.preventDefault();
     var modalOverlay = $('.modal__overlay');
