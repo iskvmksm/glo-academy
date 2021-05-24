f8d2a47604622b06ad7ea464b8e62d7f02f9fd83
iskvmksm <isakovmaksim777@gmail.com>
2021-05-24 17:30:35 +0600

    create: validation form and include mask

--- js/main.js
+++ js/main.js
@@ -67,8 +67,41 @@ $(document).ready(function() {
         return false;
       }
   });
+// Обработка форм
+$('.form').each(function(){
+  $(this).validate({
+    errorClass: "invalid",
+    messages: {
+      name: {
+        required: "Укажите имя",
+        minlength: "Имя должно быть не короче 2 букв",
+      },
+      email: {
+        required: "We need your email address to contact you",
+        email: "Your email address must be in the format of name@domain.com"
+      },
+      phone: {
+        required: "Телефон обязателен",
+      }
+    },
+  });
+})
+var options =  {
+  // onComplete: function(cep) {
+  //   alert('CEP Completed!:' + cep);
+  // },
+  onKeyPress: function(cep, event, currentField, options){
+    console.log('A key was pressed!:', cep, ' event: ', event,
+                'currentField: ', currentField, ' options: ', options);
+  },
+  onChange: function(cep){
+    console.log('cep changed! ', cep);
+  },
+  onInvalid: function(val, e, f, invalid, options){
+    var error = invalid[0];
+    console.log ("Digit: ", error.v, " is invalid for the position: ", error.p, ". We expect something like: ", error.e);
+  }
+};
 
-$('.close-modal').click(function(){
-    elements.removeClass('active');
-});
+$('.cep_with_callback').mask('+7 (000) 000-00-00', options);
 });
