194e573c16445e72dd30a4a24d7c63aecf1246c1
iskvmksm <isakovmaksim777@gmail.com>
2021-05-25 12:14:29 +0600

    create: added elements animation

--- js/main.js
+++ js/main.js
@@ -67,42 +67,43 @@ $(document).ready(function() {
         return false;
       }
   });
-// Обработка форм
-$('.form').each(function(){
-  $(this).validate({
-    errorClass: "invalid",
-    messages: {
-      name: {
-        required: "Your name",
-        minlength: "Name must be at least 2 letters long",
+  // Обработка форм
+  $('.form').each(function(){
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Your name",
+          minlength: "Name must be at least 2 letters long",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Required format name@domain.com"
+        },
+        phone: {
+          required: "Phone is required",
+          minlength: "Phone number must be at least 11 characters",
+        }
       },
-      email: {
-        required: "We need your email address to contact you",
-        email: "Your email address must be in the format of name@domain.com"
-      },
-      phone: {
-        required: "Phone is required",
-        minlength: "Phone number must be at least 11 characters",
-      }
+    });
+  })
+  var options =  {
+    // onComplete: function(cep) {
+    //   alert('CEP Completed!:' + cep);
+    // },
+    onKeyPress: function(cep, event, currentField, options){
+      console.log('A key was pressed!:', cep, ' event: ', event,
+                  'currentField: ', currentField, ' options: ', options);
     },
-  });
-})
-var options =  {
-  // onComplete: function(cep) {
-  //   alert('CEP Completed!:' + cep);
-  // },
-  onKeyPress: function(cep, event, currentField, options){
-    console.log('A key was pressed!:', cep, ' event: ', event,
-                'currentField: ', currentField, ' options: ', options);
-  },
-  onChange: function(cep){
-    console.log('cep changed! ', cep);
-  },
-  onInvalid: function(val, e, f, invalid, options){
-    var error = invalid[0];
-    console.log ("Digit: ", error.v, " is invalid for the position: ", error.p, ". We expect something like: ", error.e);
-  }
-};
+    onChange: function(cep){
+      console.log('cep changed! ', cep);
+    },
+    onInvalid: function(val, e, f, invalid, options){
+      var error = invalid[0];
+      console.log ("Digit: ", error.v, " is invalid for the position: ", error.p, ". We expect something like: ", error.e);
+    }
+  };
 
-$('.cep_with_callback').mask('+7 (000) 000-00-00', options);
+  $('.cep_with_callback').mask('+7 (000) 000-00-00', options);
+  AOS.init();
 });
